f002c6f5c4715c4726bd806bc3715571915c28ef
misawa-san <hiroshi.no.mobile@gmail.com>
2023-08-18 15:09:06 +0900

    timeout change

--- robot/entrypoints/docker-entrypoint.sh
+++ robot/entrypoints/docker-entrypoint.sh
@@ -3,6 +3,6 @@
 cd result/SILS/reslt_CheckLightSensor/
 python3 make_testsuite.py
 
-sleep 30
+sleep 60
 
 robot  --xunit xunit_report.xml -i SILS  -v xils:SILS -P ../../../testsuites/CheckLightSensor/s_CheckLightSensor/Scripts  -P ../../../testsuites/CheckLightSensor/t_CheckLightSensor/Scripts ../../../testsuites/CheckLightSensor
