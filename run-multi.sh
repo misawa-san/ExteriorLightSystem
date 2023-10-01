feeee32e20beb2fa61553b62e9cb52dec572bda8
Hiroshi Misawa <51118192+misawa-san@users.noreply.github.com>
2023-10-01 18:53:58 +0900

    Create run-multi.sh

---
+++ run-multi.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ソースディレクトリとターゲットディレクトリのパスを指定
+source_dir="./robot/testware_lib/s_CheckLightSensor/Scripts/ready"
+target_dir="./robot/testware_lib/s_CheckLightSensor/Scripts/"
+
+# ソースディレクトリ内のすべての.pyファイルを処理
+for source_file in "$source_dir"/*.py; do
+    if [ -f "$source_file" ]; then
+        # ファイル名を取得
+        file_name=$(basename "$source_file")
+
+        # ファイルをターゲットディレクトリにコピー
+        cp "$source_file" "$target_dir/s_lib_sils.py"
+
+        # コマンドを実行
+        ./run-single.sh
+
+    fi
+done
+
+basename="artifact_"
+postfix=$(date "+%Y%m%d-%H%M%S")
+artifact_name="$basename$postfix.zip"
+zip -r "$artifact_name" .
