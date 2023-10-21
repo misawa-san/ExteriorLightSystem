29274d29a902bbc17cc383e087b47df4df94d7ee
Hiroshi Misawa <51118192+misawa-san@users.noreply.github.com>
2023-10-22 07:23:43 +0900

    Update run-single.sh

--- run-single.sh
+++ run-single.sh
@@ -8,7 +8,7 @@ docker compose -f docker-compose.yml up -d --build
 monitor_dir="./FrontSpotLights/src"
 
 # タイムアウトの秒数
-timeout_seconds=18000
+timeout_seconds=180000
 
 # タイムアウト時のメッセージ
 timeout_message="timeout occured. check your source code"
