9fd1238eec7f5155832429a4fc117bdfee87379d
Hiroshi Misawa <51118192+misawa-san@users.noreply.github.com>
2023-10-22 10:50:01 +0900

    Update mmap.h

--- ExteriorLightController/src/src/pubsub/src/mclient/user/mmap.h
+++ ExteriorLightController/src/src/pubsub/src/mclient/user/mmap.h
@@ -1,16 +1,17 @@
 #ifndef _MMAP_H_
-#define _MMAP_H_
 
 #include <stdio.h>
 
 /* ------------------ USER CUSTOIMIZE ----------------------------*/
-#define VAL_LIST_NUM	(14)
+#define VAL_LIST_NUM (14)
 /* ------------------ USER CUSTOIMIZE ----------------------------*/
 
 struct Val
 {
 	unsigned long addr;
+	unsigned char size;
 	unsigned long mask;
+	char name[64];
 };
 
 extern struct Val val_list[VAL_LIST_NUM];
